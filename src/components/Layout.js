b214e5518115bb9ebb580a08c8d89b5c508368df
Nichele Dunn <the7yearitch@hotmail.com>
2021-08-20 14:34:04 -0600

    ♻️ Fix file import

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 const Layout = ({ children }) => {
   return (
