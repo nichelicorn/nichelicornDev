e2c505d2ca74ada1cdd2c03757074d91cdde5817
Nichele Dunn <the7yearitch@hotmail.com>
2021-08-22 10:50:52 -0600

    ♻️  Format spacing in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,27 @@ import Layout from "../components/Layout";
 import "../components/layout.css";
 
 // styles
+
 // const pageStyles = {
 //   color: "#232129",
 //   padding: 96,
 //   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 // }
+
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
 }
+
 const headingAccentStyles = {
   color: "#663399",
 }
+
 const paragraphStyles = {
   marginBottom: 48,
 }
+
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
@@ -26,10 +31,12 @@ const codeStyles = {
   fontSize: "1.25rem",
   borderRadius: 4,
 }
+
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
 }
+
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
@@ -127,9 +134,8 @@ const links = [
   },
 ]
 
-// markup
+// Component to render
 const IndexPage = () => {
-  // this IndexPage is a React component to render the whole page
   return (
     <Layout>
       <title>Home Page</title>
