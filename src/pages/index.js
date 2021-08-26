610b20c47097f0b01a26a63cce3b41ace774b0e1
Nichele Dunn <the7yearitch@hotmail.com>
2021-08-26 15:02:35 -0600

    ♻️  Create directory for styles and update links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
 import Layout from "../components/Layout";
-import "../components/layout.css";
-import "../components/header.css";
-import "../components/footer.css";
+import "../styles/layout.css";
+import "../styles/header.css";
+import "../styles/footer.css";
 
 // styles
 
