a0a9397df32e6da82cabe8d76d48fe65bb17a3f8
Nichele Dunn <the7yearitch@hotmail.com>
2021-08-31 15:21:26 -0600

    ✨ Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <title>nichelicornDev</title>
+      <h1>🌊Welcome to nichelicorn.dev👩‍💻</h1>
+      <p>This is where I share code and writings about technology, make random observations about life, and detail the exploits of my lively fish!</p>
+      <aside>The exploits of the fish mainly involve pooping and eating...</aside>
+      <p>Anywhoo, this will all be coming along incrementally as I work on my developer skills and practice JavaScript, HTML, CSS, and dive into learning more technologies!</p>
+      <p>Currently, I am working on 👩‍💻 this website, nichelicorn.dev, a hotel management app with an API, and learning how to rollerskate.</p>
+    </section>
+  )
+}
+
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from "../components/Layout";
+import Home from "../components/Home";
 import "../styles/typography.css";
 import "../styles/layout.css";
 import "../styles/header.css";
@@ -17,12 +18,7 @@ import "../styles/footer.css";
 const IndexPage = () => {
   return (
     <Layout>
-      <title>nichelicornDev</title>
-      <h1>🌊Welcome to nichelicorn.dev👩‍💻</h1>
-      <p>This is where I share code and writings about technology, make random observations about life, and detail the exploits of my lively fish!</p>
-      <aside>The exploits of the fish mainly involve pooping and eating...</aside>
-      <p>Anywhoo, this will all be coming along incrementally as I work on my developer skills and practice JavaScript, HTML, CSS, and dive into learning more technologies!</p>
-      <p>Currently, I am working on 👩‍💻 this website, nichelicorn.dev, a hotel management app with an API, and learning how to rollerskate.</p>
+      <Home />
     </Layout>
   )
 }
