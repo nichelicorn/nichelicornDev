81a678ff585cf8dc7a12e1e392b98b620cb57cb2
Nichele Dunn <the7yearitch@hotmail.com>
2021-08-31 15:14:53 -0600

    ♻️  Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,23 @@ import "../styles/header.css";
 import "../styles/footer.css";
 
 // Styles
-const headingStyles = {
-  margin: 33,
-  // marginBottom: 64,
-  textAlign: "center",
-  // maxWidth: 320,
-}
+// const headingStyles = {
+//   margin: 33,
+//   // marginBottom: 64,
+//   textAlign: "center",
+//   // maxWidth: 320,
+// }
 
 // Component to render
 const IndexPage = () => {
   return (
     <Layout>
       <title>nichelicornDev</title>
-      <h1 style={headingStyles}>
-      🌊 Welcome to nichelicorn.dev 👩‍💻
-        <br />
-      </h1>
+      <h1>🌊Welcome to nichelicorn.dev👩‍💻</h1>
+      <p>This is where I share code and writings about technology, make random observations about life, and detail the exploits of my lively fish!</p>
+      <aside>The exploits of the fish mainly involve pooping and eating...</aside>
+      <p>Anywhoo, this will all be coming along incrementally as I work on my developer skills and practice JavaScript, HTML, CSS, and dive into learning more technologies!</p>
+      <p>Currently, I am working on 👩‍💻 this website, nichelicorn.dev, a hotel management app with an API, and learning how to rollerskate.</p>
     </Layout>
   )
 }
