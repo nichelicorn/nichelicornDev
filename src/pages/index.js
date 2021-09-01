d009c6da4dbe8e85e022c9f4a4e4dc950609b507
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-01 17:11:11 -0600

    ♻️  Delete typography file and import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/Layout";
 import Home from "../components/Home";
 import "@fontsource/beth-ellen";
-import "../styles/typography.css";
 import "../styles/layout.css";
 import "../styles/header.css";
 import "../styles/home.css";
