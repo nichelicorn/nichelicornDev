b3513d8449a91440e60c82452d73e2cd474f27cf
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-01 14:45:05 -0600

    💅 Update Home page styles

--- src/components/SideContent.js
+++ src/components/SideContent.js
@@ -6,8 +6,8 @@ const SideContent = () => {
   return (
     <div className="sideContent">
       <h2 className="sideHeader">🦄</h2>
-      <article>
-        <a class="twitter-timeline" data-dnt="true" data-theme="dark" href="https://twitter.com/nichelicorn?ref_src=twsrc%5Etfw">Tweets by nichelicorn</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+      <article className="tweet-stream">
+        <a className="twitter-timeline" data-dnt="true" data-theme="dark" href="https://twitter.com/nichelicorn?ref_src=twsrc%5Etfw">Tweets by nichelicorn</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
       </article>
       {/* <p>This is where I share code and write about technology, random observations, and the exploits of my lively fish!</p>
       <aside>The exploits of the fish mainly involve pooping and eating...🐟💩</aside>
