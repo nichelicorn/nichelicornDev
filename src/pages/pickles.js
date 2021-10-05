cbcbe1ac470b19c0047b11bfef253d9f6c8ff925
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-05 15:29:10 -0600

    ✨ Add hook to fetch Nichelicorn GitHub data

--- src/pages/pickles.js
+++ src/pages/pickles.js
@@ -2,20 +2,41 @@
 // import React from "react";
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout";
-import { Link, useStaticQuery, graphql } from "gatsby";
-// import repos from "../data/repos.js";
+import { Link } from "gatsby";
+// import { Link, useStaticQuery, graphql } from "gatsby";
+import repos from "../data/repos.js";
 // import pickleData from "../data/pickleData.js";
 
 const PicklePage = () => {
   // console.log("repos <>>>", repos);
 
-  const [ starsCount, setStarsCount ] = useState(0)
+  // const [ starsCount, setStarsCount ] = useState(0)
+  // useEffect(() => {
+  //   // get data from GitHub api
+  //   fetch("https://api.github.com/repos/gatsbyjs/gatsby")
+  //     .then(response => response.json()) // parse JSON data from request
+  //     .then(resultData => {
+  //       setStarsCount(resultData.stargazers_count) // set data for the number of stars
+  //     })
+  // }, [])
+
+    const [ starsCount, setStarsCount ] = useState(0)
   useEffect(() => {
     // get data from GitHub api
     fetch("https://api.github.com/repos/gatsbyjs/gatsby")
       .then(response => response.json()) // parse JSON data from request
       .then(resultData => {
         setStarsCount(resultData.stargazers_count) // set data for the number of stars
+      })
+  }, [])
+
+  const [ nichelicornRepos, setNichelicornRepos ] = useState(0)
+  useEffect(() => {
+    // get data from GitHub api
+    fetch("https://api.github.com/users/nichelicorn/repos")
+      .then(response => response.json()) // parse JSON data from request
+      .then(resultData => {
+        setNichelicornRepos(resultData) // sets repo data
       })
   }, [])
 
@@ -28,9 +49,11 @@ const PicklePage = () => {
   <Layout>
     {/* {console.log("repos??", repos)} */}
     {/* {console.log(repos.map(repo => repo.name))} */}
-    {/* {console.log(repos[0].languages)} */}
-    {/* {console.log(nichelicornGH)} */}
+    
     {console.log("starsCount <>>>", starsCount)}
+
+    {console.log("nichelicornRepos <>>>", nichelicornRepos)}
+
     {
       repos.map(repo => {
         return (
