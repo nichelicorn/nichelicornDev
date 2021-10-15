baf2a655f024a564ab1abcd109164c05932a6daa
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-15 16:16:01 -0600

    ♻️  Comment out console logs

--- src/pages/posts/{mdx.slug}.js
+++ src/pages/posts/{mdx.slug}.js
@@ -5,16 +5,14 @@ import Layout from "../../components/Layout";
 
 const Post = ({ data }) => {
 
-  console.log("post data <>>>", data);
+  // console.log("post data <>>>", data);
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      {/* <p>Content will *eventually* be displayed here.</p> */}
-      {/* <p>{data.mdx.frontmatter.date}</p> */}
       <article className="post-wrapper">
         <section  className="post-styles">
           <MDXRenderer>
-              {data.mdx.body}
+            {data.mdx.body}
           </MDXRenderer>
         </section>
       </article>  
